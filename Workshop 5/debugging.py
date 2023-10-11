44f9cfddd20e1ddd3a66ac979dadb4b9dd404440
keeratiburt <125423996+keeratiburt@users.noreply.github.com>
2023-10-11 12:29:21 +0700

    Create debugging.py

---
+++ Workshop 5/debugging.py
@@ -0,0 +1,38 @@
+########## attribute error ##########
+'''
+# AttributeError
+ 
+X = 10
+ 
+# Raises an AttributeError
+X.append(6)
+
+##################
+#x = [10, 11]
+ 
+# Raises an AttributeError
+#x.append(6)
+#print(x)
+
+'''
+
+
+########## IndexError ##########
+'''
+j = [1, 2, 4]
+print(j[4])
+'''
+
+########## KeyError ##########
+'''
+student = {
+  "name": "John",
+  "course": "Python",
+}
+
+print(student["age"])
+'''
+
+x=10 
+y="Hello World" 
+print(x+y) 
